d12b35f7f2d2b8bee4591f2c80f3fb803dfa0754
amoretspero <amoretspero@gmail.com>
2015-04-04 14:21:25 +0900

    Initial Commit.

---
+++ ConsoleApplication1/File1.fs
@@ -0,0 +1,20 @@
+﻿open System
+open System.Collections
+open System.Diagnostics
+open System.IO
+open System.Linq
+open System.Text
+open System.Drawing
+open System.Drawing.Design
+open System.Threading
+open System.Windows.Forms
+
+/// Fonts. - Must include : font name, font size, FontStyle, GraphicsUnit
+let font_title = new Font("나눔손글씨 펜", 14.0f, FontStyle.Regular, GraphicsUnit.Point)
+let font_text = new Font("맑은 고딕", 10.0f, FontStyle.Regular, GraphicsUnit.Point)
+let font_label = new Font("서울남산체", 10.0f, FontStyle.Regular, GraphicsUnit.Point)
+
+/// Forms for slide_test, test_selections. - Must include : Text, Width, Height, StartPosition, AutoScaleMode, FormBorderStyle
+let form_slide_test = new Form(Width = 1280, Height = 720, StartPosition = FormStartPosition.CenterScreen, AutoScaleMode = AutoScaleMode.Font, FormBorderStyle = FormBorderStyle.FixedSingle, Text = "Slide Test")
+
+let 
---
+++ ConsoleApplication1/File2.fs
@@ -0,0 +1,4 @@
+﻿module File2
+
+open System.Drawing
+open System.Drawing.Drawing2D
